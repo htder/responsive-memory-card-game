2ec3458f06cce3d9266c6bc3b707134a48313b0d
Henry Derbyshire <henry@htderbyshire.com>
2022-09-23 12:58:07 +0100

    added basic bootstrap styling to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
 function Header() {
-  return <h1>Header</h1>
+  return <div className="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <span className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">Header</span>
+      <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li className="nav-item px-2">Current Score</li>
+        <li className="nav-item px-2">High Score</li>
+      </ul>
+      <div className="text-end">
+        <button type="button" className="btn btn-outline-primary me-2">Rules</button>
+        <button type="button" className="btn btn-outline-primary me-2">Difficulty</button>
+        <button type="button" className="btn btn-outline-primary me-2">Reset</button>
+
+      </div>
+    </header>
+  </div>
 }
 
 export default Header;
