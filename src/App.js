c5427e65501d1a7ba77f9682d18fd643351b334b
Henry Derbyshire <henry@htderbyshire.com>
2022-09-23 16:59:34 +0100

    added state and gameplay functions to App.js, now passing score data as props to Header.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,151 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from "./components/Header"
 import Card from "./components/Card"
+import COLOURS from "./data.json"
 
 function App() {
+  const colourData = COLOURS.colours;
+  const [scores, setScores] = useState({
+    hasWon: false,
+    currentScore: 0,
+    highScore: 0
+  });
+  const [difficulty, setDifficulty] = useState("easy");
+  const [choices, setChoices] = useState([]);
+  const [cards, setCards] = useState(createColourCards());
+
+  useEffect(() => {
+    setCards(createColourCards());
+    setChoices([]);
+  }, [difficulty]);
+
+  useEffect(() => {
+    if (choices.length === cards.length) {
+      setScores((prev) => ({
+        hasWon: true,
+        currentScore: 0,
+        highScore: prev.highScore > scores.currentScore ? prev.highScore : scores.currentScore,
+      }));
+    }
+  }, [choices]);
+
+  useEffect(() => {
+    if (scores.hasWon) {
+      setCards(createColourCards());
+      setChoices([]);
+    }
+  }, [scores]);
+
+  function createColourCards() {
+    let colourAmount;
+    if (difficulty === "easy") colourAmount = 5;
+    if (difficulty === "medium") colourAmount = 10;
+    if (difficulty === "hard") colourAmount = 20;
+    const randomColours = [];
+    const keys = Object.keys(colourData);
+
+    while (randomColours.length < colourAmount) {
+      const colour = colourData[keys[keys.length * Math.random() << 0]];
+      if (randomColours.indexOf(colour) === -1) {
+        randomColours.push(colour);
+      }
+    }
+    return randomColours;
+  }
+
+  function shuffleCards() {
+    const newCardOrder = [...cards];
+    for (let i = newCardOrder.length - 1; i > 0; i--) {
+      const randValue = Math.floor(Math.random() * i);
+      const tempValue = newCardOrder[i];
+      newCardOrder[i] = newCardOrder[randValue];
+      newCardOrder[randValue] = tempValue;
+    }
+    setCards(newCardOrder);
+  }
+
+  function isAlreadyChosen(id) {
+    for (let i = 0; i < choices.length; i++) {
+      if (choices[i] === id) {
+        return true;
+      }
+    }
+    return false; 
+  }
+
+  function handleCardClick(id) {
+    const alreadyChosen = isAlreadyChosen(id);
+    if (!alreadyChosen) {
+      changeScore(!alreadyChosen);
+      shuffleCards();
+      setChoices([
+        ...choices,
+        id
+      ])
+      return;
+    }
+    if (alreadyChosen) {
+      changeScore(!alreadyChosen);
+      setCards(createColourCards());
+      setChoices([]);
+    }
+  }
+
+  function handleClickDiff(level) {
+    setDifficulty(level);
+    setScores(prevScores => ({
+      hasWon: false,
+      highScore: prevScores.highScore,
+      currentScore: 0
+    }))
+  }
+
+  function changeScore(correct) {
+    if (correct) {
+      const newScore = scores.currentScore + 1;
+      setScores((prevScores) => ({
+        hasWon: prevScores.hasWon,
+        currentScore: newScore,
+        highScore: prevScores.highScore < newScore 
+          ? newScore 
+          : prevScores.highScore 
+      }));
+    } else {
+      setScores((prevScores) => ({
+        hasWon: prevScores.hasWon,
+        currentScore: 0,
+        highScore: prevScores.highScore < prevScores.currentScore 
+          ? prevScores.currentScore 
+          : prevScores.highScore 
+      }));
+    }
+  }
+
+  function playAgain() {
+    setScores(prevScores => ({
+      hasWon: false,
+      currentScore: 0,
+      highScore: prevScores.highScore
+    }))
+    setChoices([]);
+  }
+
+  const renderCards = cards.map(color => {
+    return <Card 
+              color={color.hex}
+              name={color.name}
+              key={color.hex}
+              handleCardClick={() => handleCardClick(color.hex)}
+            />
+  });
+
   return (
     <div className="container">
-      <Header/>
+      <Header
+        currentScore = {scores.currentScore}
+        highScore = {scores.highScore}
+      />
       <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 rows-cols-lg-4 row-cols-xl-5'>
         <Card />
         <Card />
