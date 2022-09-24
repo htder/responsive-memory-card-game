f8453b4ec5918e0f9e60c5a97295e1b514a232cc
Henry Derbyshire <henry@htderbyshire.com>
2022-09-24 11:41:39 +0100

    corrected spelling error, created PlayAgainModal

---
+++ src/components/DifficultyModal.js
@@ -0,0 +1,44 @@
+import "./DifficultyModal.css";
+function DifficultyModel(props) {
+  return <div 
+    onClick={() => props.setOpen(false)}
+    className="modal d-block bg-light py-5"  role="dialog" id="modalSheet"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content rounded-4 shadow">
+          <div className="modal-header border-bottom-0">
+            <h5 className="modal-title">Select a Difficulty!</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-footer flex-column border-top-0">
+            <button
+              type="button" 
+              className="btn btn-lg btn-primary w-100 mx-0 mb-2"
+              onClick={() => props.handleClick("easy")}>
+            Easy
+            </button>
+            <button 
+              type="button" 
+              className="btn btn-lg btn-primary w-100 mx-0 mb-2"
+              onClick={() => props.handleClick("medium")}>
+            Medium
+            </button>
+            <button 
+              type="button" 
+              className="btn btn-lg btn-primary w-100 mx-0 mb-2"
+              onClick={() => props.handleClick("hard")}>
+            Hard
+            </button>
+            <button
+              type="button" 
+              className="btn btn-lg btn-light w-100 mx-0" 
+              data-bs-dismiss="modal">
+            Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+}
+
+export default DifficultyModel;
--- src/components/PlayAgainModal.js
+++ src/components/PlayAgainModal.js
@@ -1,18 +1,22 @@
-import "./DifficultlyModal.css";
-function DifficultlyModel(props) {
+import "./DifficultyModal.css";
+function PlayAgainModal(props) {
+  let message = `You have completed the ${props.difficulty} level! Maybe try a harder difficulty this time?`
+  if (props.difficulty === "hard") {
+    message = "You have completed the hardest level! You must be very good at this game."
+  }
   return <div 
-    onClick={() => props.setOpen(false)}
+    onClick={() => props.handleClick(props.difficulty)}
     className="modal d-block bg-light py-5"  role="dialog" id="modalSheet"
     >
       <div className="modal-dialog" role="document">
         <div className="modal-content rounded-4 shadow">
           <div className="modal-header border-bottom-0">
-            <h5 className="modal-title">Select a Difficulty!</h5>
+            <h5 className="modal-title">Congratulations!</h5>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-          {/* <div className="modal-body py-0"> */}
-          {/*   <p>This is a modal sheet, a variation of the modal that docs itself to the bottom of the viewport like the newer share sheets in iOS.</p> */}
-          {/* </div> */}
+          <div className="modal-body py-0">
+            <p>{message}</p>
+          </div>
           <div className="modal-footer flex-column border-top-0">
             <button
               type="button" 
@@ -44,4 +48,4 @@ function DifficultlyModel(props) {
     </div>
 }
 
-export default DifficultlyModel;
+export default PlayAgainModal;
